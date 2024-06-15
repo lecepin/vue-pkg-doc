fe6c8ee2138f88cef57674104f032898cbdf025a
lecepin <383810086@qq.com>
2024-06-15 11:02:01 +0800

    docs: font

--- packages/docs/.vitepress/theme/index.ts
+++ packages/docs/.vitepress/theme/index.ts
@@ -1,6 +1,7 @@
 import type { Theme } from 'vitepress'
 import DefaultTheme from 'vitepress/theme'
 import DemoPreview, { useComponents } from 'vitepress-code-demo-container'
+import 'cn-fontsource-lxgw-wen-kai-gb-screen-r/font.css'
 import 'vitepress-code-demo-container/dist/style.css'
 
 import './style.css'
